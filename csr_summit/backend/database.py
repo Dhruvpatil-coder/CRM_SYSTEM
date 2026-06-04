ce94e0057807f75a0395c5ad06631a0df9d59d7a
Nayan Suresh Andhale <andhalenayan67@gmail.com>
2026-06-04 19:59:42 +0530

    Update database.py

--- csr_summit/backend/database.py
+++ csr_summit/backend/database.py
@@ -6,8 +6,7 @@ DATA_FILE = "data.json"
 def load_data():
     if not os.path.exists(DATA_FILE):
         save_data({
-            "pre_registered": [], "walk_ins": [], "speakers": [],
-            "counters": {"pre_individual": 0, "pre_group": 0, "walkin_individual": 0, "walkin_group": 0}
+            "pre_registered": [], "walk_ins": [], "speakers": [], "groups": []
         })
     with open(DATA_FILE, "r") as f:
         return json.load(f)
