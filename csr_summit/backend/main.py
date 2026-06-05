56cd7922b3da431a00eb26fbf3f1bc74ed500432
Dhruv <dhruvpatil824@gmail.com>
2026-06-05 08:10:36 +0530

    update main.py

--- csr_summit/backend/main.py
+++ csr_summit/backend/main.py
@@ -6,7 +6,7 @@ app = FastAPI(title="CSR Summit CRM")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://127.0.0.1:5500", "http://localhost:5500"],
+    allow_origins=["https://csrsummit2.netlify.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
